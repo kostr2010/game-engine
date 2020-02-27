2918df928be796c7b6597c79b4acfa2c12376815
levabala <levabala@ya.ru>
2020-02-27 21:36:42 +0300

    formatting

--- engine-types.h
+++ engine-types.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <vector>
-#include <string>
 #include <algorithm>
 #include <iostream>
 #include <map>
+#include <string>
+#include <vector>
 
 // to print enums
 #define EnumToString(var) #var
@@ -13,81 +13,88 @@
 // ENUMS
 
 enum AbilityKind {
-    CanDie,             // is tough. can be damaged or healed
-    CanContain,         // may contain something pickable    
-    CanBePicked,        // may be picked up
-    CanMove,            // ability to move to another tile
-    CanPick,            // can pick pickable                    | FIXME
-    CanLoot,            // can pick subEntities of CanContain   | 
-    CanKick,            // ability to impact physical damage to another person
-    CanHack,            // can unlock CanBeLocked instance
-    CanBeLocked,        // can be locked
+    CanDie,      // is tough. can be damaged or healed
+    CanContain,  // may contain something pickable
+    CanBePicked, // may be picked up
+    CanMove,     // ability to move to another tile
+    CanPick,     // can pick pickable                    | FIXME
+    CanLoot,     // can pick subEntities of CanContain   |
+    CanKick,     // ability to impact physical damage to another person
+    CanHack,     // can unlock CanBeLocked instance
+    CanBeLocked, // can be locked
 };
 
 enum AbilityState {
-    DamageAmount,       // amount of damage dealt per kick | FIXME separate damage types / sources
-    HpMax,              // max amount of hp entity has
-    HpCurrent,          // current amount of hp entity has
-    ContainCapacity,    // max capacity of the subentities_ vector
-    SpotToLoot,         // index of target.subentities_ vector to loot
-    LockLevel,          // lock level [0; n]
-                        // warrior uses his weapon to open locked chests at the price of their durability, rogue uses lockpicks, wizzard uses magic at the cost of the chance that some loot might be destroyed
-    HackLevel,          // if HackLevel >= LockLevel, then can hack
+    DamageAmount,    // amount of damage dealt per kick | FIXME separate damage
+                     // types / sources
+    HpMax,           // max amount of hp entity has
+    HpCurrent,       // current amount of hp entity has
+    ContainCapacity, // max capacity of the subentities_ vector
+    SpotToLoot,      // index of target.subentities_ vector to loot
+    LockLevel,       // lock level [0; n]
+                     // warrior uses his weapon to open locked chests at the price of
+                     // their durability, rogue uses lockpicks, wizzard uses magic at
+                     // the cost of the chance that some loot might be destroyed
+    HackLevel,       // if HackLevel >= LockLevel, then can hack
 };
 
 //====================
 // CLASSES DECLARATION
 
 class Ability {
-public:
+  public:
     Ability();
     Ability(AbilityKind kind, std::map<AbilityState, int> init_state);
 
     int  GetStateValue(AbilityState name);
     void SetStateValue(AbilityState name, int new_value);
 
-    friend bool          operator==(const Ability& left, const Ability& right);
-    friend std::ostream& operator<<(std::ostream& stream, Ability ability);
+    friend bool          operator==(const Ability &left, const Ability &right);
+    friend std::ostream &operator<<(std::ostream &stream, Ability ability);
 
     AbilityKind kind_;
-private:
+
+  private:
     std::map<AbilityState, int> state_;
-protected:
+
+  protected:
 };
 
 class Entity {
-public:
+  public:
     Entity();
     Entity(std::vector<Ability> abilities);
 
-    void   Apply(AbilityKind kind, Entity& target);
+    void   Apply(AbilityKind kind, Entity &target);
     int    InventoryGetSize();
     void   InventoryAdd(std::vector<Entity> items);
     void   InventoryRemove(size_t index);
     Entity InventoryGetSubentity(size_t index);
 
-    friend std::ostream& operator<<(std::ostream& stream, Entity entity);
+    friend std::ostream &operator<<(std::ostream &stream, Entity entity);
 
     std::map<AbilityKind, Ability> abilities_;
-private:
+
+  private:
     std::vector<Entity> subentities_;
-protected:
+
+  protected:
 };
 
 class WorldMap {
-public:
-private:
-protected:
+  public:
+  private:
+  protected:
 };
 
 class Tile {
-public:
-private:
-protected:
+  public:
+  private:
+  protected:
 };
 
 class EntityFactory {
-public:
+  public:
     // playable characters
     static Entity CreateWarrior();
     static Entity CreateMage();
@@ -101,21 +108,23 @@ public:
 
     // items
     static Entity CreateFood();
-private:
-protected:
+
+  private:
+  protected:
 };
 
 class AbilityFactory {
-public:    
-    static Ability CreateAbilityDie(size_t hp_max, size_t hp_current);  //
-    static Ability CreateAbilityKick(size_t damage);                    //
-    static Ability CreateAbilityPick();                                 // can pick CanBePicked entities (not subEntites)
-    static Ability CreateAbilityLoot();                                 // int subEntityInstanceToLoot
-    static Ability CreateAbilityBePicked();                             // no state
-    static Ability CreateAbilityContain(size_t capacity);               //
-    static Ability CreateAbilityMove();                                 // int distance, int (?)fatigure
-    static Ability CreateAbilityHack(size_t hack_level);                // int (?)hackChance
-    static Ability CreateAbilityBeLocked(size_t lock_level);            // bool initialLocked
-private:    
-protected:
+  public:
+    static Ability CreateAbilityDie(size_t hp_max, size_t hp_current);
+    static Ability CreateAbilityKick(size_t damage);
+    static Ability CreateAbilityPick();
+    static Ability CreateAbilityLoot();
+    static Ability CreateAbilityBePicked();
+    static Ability CreateAbilityContain(size_t capacity);
+    static Ability CreateAbilityMove();
+    static Ability CreateAbilityHack(size_t hack_level);
+    static Ability CreateAbilityBeLocked(size_t lock_level);
+
+  private:
+  protected:
 };
