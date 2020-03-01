ecb0da724c2d4cc815e6510839358f68f09fc1a9
levabala <levabala@ya.ru>
2020-03-01 16:58:47 +0300

    dependencies installing script

---
+++ install_deps.sh
@@ -0,0 +1,3 @@
+mkdir -p libs/catch2
+curl -o libs/catch2/catch2.hpp https://github.com/catchorg/Catch2/blob/master/single_include/catch2/catch.hpp
+git clone https://github.com/r-lyeh-archived/sole libs/sole
